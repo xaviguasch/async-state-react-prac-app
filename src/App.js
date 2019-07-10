61adc4238637aa5df57ffa8e4262bfd1952b1e56
xaviguasch <xaviguasch@gmail.com>
2019-07-10 14:34:51 +0200

    toyed with prevState and prevProps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      meaningOfLife: 47
+    }
+  }
+
+  handleClick = () => {
+    this.setState((prevState, prevProps) => {
+      return { meaningOfLife: prevState.meaningOfLife + 1 }
+    })
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>{this.state.meaningOfLife}</p>
+          <button onClick={this.handleClick}>Update State</button>
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
